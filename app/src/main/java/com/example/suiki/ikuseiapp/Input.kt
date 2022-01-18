f901197598f2744672db222f3c95202fa0d55e0b
sekiyan372 <b1917181@planet.kanazawa-it.ac.jp>
2022-01-19 05:56:21 +0900

    ファイルを整理

--- app/src/main/java/com/example/suiki/ikuseiapp/Input.kt
+++ app/src/main/java/com/example/suiki/ikuseiapp/Input.kt
@@ -16,7 +16,7 @@ import java.time.format.DateTimeFormatter
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-class input : Fragment() {
+class Input : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -68,7 +68,7 @@ class input : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            input().apply {
+            Input().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
