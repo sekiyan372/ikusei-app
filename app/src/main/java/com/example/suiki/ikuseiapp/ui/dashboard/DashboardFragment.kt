024bec65802d6844c0236f56aac8dd17fc8156aa
sekiyan372 <b1917181@planet.kanazawa-it.ac.jp>
2022-01-18 20:55:54 +0900

    キャラクター一覧以外の画面遷移を全て追加

--- app/src/main/java/com/example/suiki/ikuseiapp/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/suiki/ikuseiapp/ui/dashboard/DashboardFragment.kt
@@ -4,20 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.suiki.ikuseiapp.R
 import com.example.suiki.ikuseiapp.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
-
-    private lateinit var dashboardViewModel: DashboardViewModel
     private var _binding: FragmentDashboardBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -25,17 +18,16 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
-
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        return binding.root
+    }
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.inputButton.setOnClickListener {
+            findNavController().navigate(R.id.action_dashboardFragment_to_inputFragment)
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/suiki/ikuseiapp/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/suiki/ikuseiapp/ui/notifications/NotificationsFragment.kt
@@ -4,20 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.suiki.ikuseiapp.R
 import com.example.suiki.ikuseiapp.databinding.FragmentNotificationsBinding
 
 class NotificationsFragment : Fragment() {
-
-    private lateinit var notificationsViewModel: NotificationsViewModel
     private var _binding: FragmentNotificationsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -25,13 +18,16 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
-
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        return binding.root
+    }
 
-        return root
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.setTargetButton.setOnClickListener {
+            findNavController().navigate(R.id.action_notificationsFragment_to_settingFragment)
+        }
     }
 
     override fun onDestroyView() {
